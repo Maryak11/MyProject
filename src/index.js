7d23ce64d5819b9c8f03dc143a666852eff79f14
Maryak11 <gurev.matvei@gmail.com>
2021-06-22 12:11:13 +0300

    delete dist

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './scss/style.scss'
+
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+//
+// const newSlider = document.querySelector(".swiper-container")
+
+new Swiper(".main-double-center", {
+    pagination: {
+        el: '.swiper-pagination'
+    }
+
+})
---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+
+    entry: {
+        app: './src/index.js',
+    },
+
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: 'Production',
+    //     }),
+    // ],
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 name: 'img/[name].[ext]',
+            //             }
+            //         }
+            //     ]
+            // },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ]
+};
