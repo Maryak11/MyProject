54c869dd6c163410c3ad9765c71993ee3490ccc4
Maryak11 <gurev.matvei@gmail.com>
2021-06-23 21:15:02 +0300

    add swiper.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import './scss/style.scss'
 
-import Swiper from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar } from 'swiper/core';
+
+// configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination, Scrollbar]);
 import 'swiper/swiper-bundle.css';
 //
 // const newSlider = document.querySelector(".swiper-container")
 
-new Swiper(".main-double-center", {
+const swiper = new SwiperCore(".main-double-center", {
     pagination: {
         el: '.swiper-pagination'
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
     }
-
 })
