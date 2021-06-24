ea8e10fecd1c4b66f9404e3afde1bca9039be40b
Maryak11 <gurev.matvei@gmail.com>
2021-06-24 13:41:56 +0300

    add hamburger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './scss/style.scss'
 
 import SwiperCore, {Autoplay, Navigation, Pagination, Scrollbar} from 'swiper/core';
-
-// configure Swiper to use modules
-SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay]);
 import 'swiper/swiper-bundle.css';
-//
-// const newSlider = document.querySelector(".swiper-container")
+SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay]);
+
 
 const swiper = new SwiperCore(".main-double-center", {
     pagination: {
@@ -16,10 +13,12 @@ const swiper = new SwiperCore(".main-double-center", {
     autoplay: {
         delay: 2000,
     },
-
-
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    //     draggable: true
-    // }
 })
+
+
+    const hamburger = document.querySelector(".hamburger");
+
+    hamburger.addEventListener("click", () => {
+       debugger
+        hamburger.classList.toggle("is-active");
+    });
